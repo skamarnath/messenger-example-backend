a8e21b95a6ed27748a9d4f83ca858f281777102f
S K Amarnath <askamist@gmail.com>
2020-11-17 19:34:09 +0530

    clean up files to use eslint and es6

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express, { json } from 'express';
+import logger from 'morgan';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from './routes/index';
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(json());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-module.exports = app;
+export default app;
