4609f1ae946760b39b7051aabcbb6e17c35b322d
S K Amarnath <askamist@gmail.com>
2020-11-18 16:59:08 +0530

    send and recieve message on sockets

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,20 @@
 import { socketPassportVerify } from './passport/jwt';
+import models from './models';
 
 export default function (io) {
   io.use(socketPassportVerify).on('connection', (socket) => {
     const user = socket.handshake.user;
     if (user) {
-      socket.join(user.nickname);
+      socket.join(user.id);
+
+      socket.on('sendMessage', async ({ to, message }) => {
+        await models.Message.create({
+          fromId: user.id,
+          toId: to,
+          message
+        });
+        socket.to(to).emit('receiveMessage', { from: user.id, message });
+      });
     }
   });
 }
